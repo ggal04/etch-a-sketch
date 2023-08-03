d5a0262a2f35f1dd81c09c01695d300a963b582b
ggal04 <gianlucaegallino@gmail.com>
2023-08-03 06:04:39 -0300

    Add skeleton features

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ This is an online implementation of an Etch-a-Sketch, a classic childhood toy.
 
 TODO:
 - Extras
-- Design
 - Better README
--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ let squareSize = 40;
 let size = 16;
 let color = "#000000";
 let hold = false;
+let rainbow = false;
+let shading = false;
+let erase = false
+let colorPencil = false;
 
 
 function generateGrid(size) {
@@ -61,7 +65,60 @@ function getSize(size) {
 generateGrid(size);
 addListeners();
 
+// MODE TOGGLES
 
+const colorPicker = document.querySelector('.paintcolor');
+colorPicker.addEventListener('change', () => {
+    color = colorPicker.value;
+    rainbowButton.classList.remove('toggle');
+    colorPicker.classList.add('toggle');
+    eraser.classList.remove('toggle');
+    shadingButton.classList.remove('toggle');
+    rainbow = false;
+    shading = false;
+    erase = false;
+    colorPencil = true;
+})
+
+
+const rainbowButton = document.querySelector('.rainbow');
+rainbowButton.addEventListener('click', () => {
+    rainbowButton.classList.add('toggle');
+    colorPicker.classList.remove('toggle');
+    eraser.classList.remove('toggle');
+    shadingButton.classList.remove('toggle');
+    rainbow = true;
+    shading = false;
+    erase = false;
+    colorPencil = false;
+})
+
+const shadingButton = document.querySelector('.shading');
+shadingButton.addEventListener('click', () => {
+    rainbow = false;
+    shading = true;
+    erase = false;
+    colorPencil = false;
+    rainbowButton.classList.remove('toggle');
+    colorPicker.classList.remove('toggle');
+    eraser.classList.remove('toggle');
+    shadingButton.classList.add('toggle');
+})
+
+const eraser = document.querySelector('.eraser')
+eraser.addEventListener('click', () => {
+    color = "#ffffff";
+    rainbowButton.classList.remove('toggle');
+    colorPicker.classList.remove('toggle');
+    eraser.classList.add('toggle');
+    shadingButton.classList.remove('toggle');
+    rainbow = false;
+    shading = false;
+    erase = true;
+    colorPencil = false;
+})
+ 
+// EXTRA FEATURES
 
 const changeSize = document.querySelector('.dimensionChange')
 changeSize.addEventListener('click', () => {
@@ -79,16 +136,6 @@ changeSize.addEventListener('click', () => {
     }
 })
 
-const colorPicker = document.querySelector('.paintcolor');
-colorPicker.addEventListener('change', () => {
-    color = colorPicker.value;
-})
-
-const eraser = document.querySelector('.eraser')
-eraser.addEventListener('click', () => {
-    color = "#ffffff";
-})
-
 const clear = document.querySelector('.clear')
 clear.addEventListener('click', () => {
     const grid = document.querySelector('.grid');
@@ -98,6 +145,20 @@ clear.addEventListener('click', () => {
     squareSize = 640 / size;
     const allTheSquares = document.querySelectorAll('.square');
     allTheSquares.forEach((square) => {
-        square.style.cssText = `height: ${squareSize}px; width: ${squareSize}px`
+        square.style.cssText = `background: ${colorChange.value}; height: ${squareSize}px; width: ${squareSize}px`
     })
 })
+
+const colorChange = document.querySelector('.colorChange');
+colorChange.addEventListener('change', () => {
+    const grid = document.querySelector('.grid');
+    grid.remove();
+    generateGrid(size);
+    addListeners();
+    squareSize = 640 / size;
+    const allTheSquares = document.querySelectorAll('.square');
+    allTheSquares.forEach((square) => {
+        square.style.cssText = `background: ${colorChange.value}; height: ${squareSize}px; width: ${squareSize}px`
+    })
+})
+
